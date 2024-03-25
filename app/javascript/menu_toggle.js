6a7e7d01488984fe0b9f4798b5f9a3c1dd9dee79
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2024-03-25 10:54:10 +0100

    turbo reload fix for menu JS

--- app/javascript/menu_toggle.js
+++ app/javascript/menu_toggle.js
@@ -1,16 +1,20 @@
 
-document.addEventListener('click', function(event) {
-  const menuButton = document.querySelector('.more-button');
-  const listContainer = document.querySelector('.list-container');
-  const isClickInsideMenuButton = menuButton.contains(event.target);
-  const isClickInsideListContainer = listContainer.contains(event.target);
+document.addEventListener('turbo:load', function() {
 
-  if (!isClickInsideMenuButton && !isClickInsideListContainer && listContainer.classList.contains('active')) {
-    listContainer.classList.remove('active');
-  }
-});
+  document.addEventListener('click', function(event) {
+    const menuButton = document.querySelector('.more-button');
+    const listContainer = document.querySelector('.list-container');
+    const isClickInsideMenuButton = menuButton.contains(event.target);
+    const isClickInsideListContainer = listContainer.contains(event.target);
 
-document.querySelector('.more-button').addEventListener('click', function(event) {
-  document.querySelector('.list-container').classList.toggle('active');
-  event.stopPropagation(); 
+    if (!isClickInsideMenuButton && !isClickInsideListContainer && listContainer.classList.contains('active')) {
+      listContainer.classList.remove('active');
+    }
+  });
+
+  document.querySelector('.more-button').addEventListener('click', function(event) {
+    document.querySelector('.list-container').classList.toggle('active');
+    event.stopPropagation(); 
+  });
+
 });
