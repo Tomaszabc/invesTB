bf2b996b03cfabf1b602eb24dd8c0cd262d44d0f
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2024-03-25 08:45:19 +0100

    JS script clean coded

---
+++ app/javascript/menu_toggle.js
@@ -0,0 +1,16 @@
+
+document.addEventListener('click', function(event) {
+  const menuButton = document.querySelector('.more-button');
+  const listContainer = document.querySelector('.list-container');
+  const isClickInsideMenuButton = menuButton.contains(event.target);
+  const isClickInsideListContainer = listContainer.contains(event.target);
+
+  if (!isClickInsideMenuButton && !isClickInsideListContainer && listContainer.classList.contains('active')) {
+    listContainer.classList.remove('active');
+  }
+});
+
+document.querySelector('.more-button').addEventListener('click', function(event) {
+  document.querySelector('.list-container').classList.toggle('active');
+  event.stopPropagation(); 
+});
