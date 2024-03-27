cf966ee8e5fd807f19086fc7dbc8a689ef3b7b43
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2024-03-27 08:43:06 +0100

    login modal working in class mode. Two log in buttons working

--- app/javascript/login_modal.js
+++ app/javascript/login_modal.js
@@ -1,21 +1,27 @@
 document.addEventListener('turbo:load', attachEventListeners);
 
 function attachEventListeners() {
-  const loginButton = document.getElementById('loginButton');
   const loginModal = document.getElementById('loginModal');
-
-  if (loginButton) {
-    loginButton.addEventListener('click', function(event) {
+  const closeModalTriggers = document.querySelectorAll('.close-modal-trigger');
+  
+  document.querySelectorAll('.login-trigger').forEach(button => {
+    button.addEventListener('click', function(event) {
       event.preventDefault();
       loginModal.classList.toggle('hidden');
     });
-  }
+  });
 
+  closeModalTriggers.forEach(button => {
+    button.addEventListener('click', function(event) {
+      event.preventDefault();
+      loginModal.classList.add('hidden');
+    });
+  });
+  
   // Close modal on click outside
   document.addEventListener('click', function(event) {
-    if (!loginModal.contains(event.target) && !loginButton.contains(event.target)) {
+    if (!loginModal.contains(event.target) && !event.target.closest('.login-trigger')) {
       loginModal.classList.add('hidden');
     }
   });
 }
-
