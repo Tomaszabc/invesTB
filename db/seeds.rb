4fb37fa0cfef9d2e34da47ad9b0618641b0f22be
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2024-04-23 12:46:26 +0200

    Trix styling and javascripts added

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 Article.create([
   {
     title: "Hossa na akcjach",
-    content: "There seems to be a happy concurrence of causes in human affairs, which
+    content: ActionText::Content.new("There seems to be a happy concurrence of causes in human affairs, which
     checks the growth of trade and riches, and hinders them from being
     confined entirely to one people; as might naturally at first be dreaded from
     the advantages of an established commerce. Where one nation has gotten
@@ -23,11 +23,11 @@ Article.create([
     those countries and provinces which they have already enriched, and
     flying to others, whither they are allured by the cheapness of provisions
     and labour; till they have enriched these also, and are again banished by
-    the same causes."
+    the same causes.")
   },
   {
     title: "Bessa na akcjach",
-    content: "There seems to be a happy concurrence of causes in human affairs, which
+    content: ActionText::Content.new("There seems to be a happy concurrence of causes in human affairs, which
     checks the growth of trade and riches, and hinders them from being
     confined entirely to one people; as might naturally at first be dreaded from
     the advantages of an established commerce. Where one nation has gotten
@@ -41,6 +41,42 @@ Article.create([
     those countries and provinces which they have already enriched, and
     flying to others, whither they are allured by the cheapness of provisions
     and labour; till they have enriched these also, and are again banished by
-    the same causes."
+    the same causes.")
+  },
+  {
+    title: "Zysk na akcjach",
+    content: ActionText::Content.new("There seems to be a happy concurrence of causes in human affairs, which
+    checks the growth of trade and riches, and hinders them from being
+    confined entirely to one people; as might naturally at first be dreaded from
+    the advantages of an established commerce. Where one nation has gotten
+    the start of another in trade, it is very difficult for the latter to regain the
+    ground it has lost; because of the superior industry and skill of the former,
+    and the greater stocks, of which its merchants are possessed, and which
+    enable them to trade on so much smaller profits. But these advantages are
+    compensated, in some measure, by the low price of labour in every nation
+    which has not an extensive commerce, and does not much abound in gold
+    and silver. Manufactures, therefore gradually shift their places, leaving
+    those countries and provinces which they have already enriched, and
+    flying to others, whither they are allured by the cheapness of provisions
+    and labour; till they have enriched these also, and are again banished by
+    the same causes.")
+  },
+  {
+    title: "Strata na akcjach",
+    content: ActionText::Content.new("There seems to be a happy concurrence of causes in human affairs, which
+    checks the growth of trade and riches, and hinders them from being
+    confined entirely to one people; as might naturally at first be dreaded from
+    the advantages of an established commerce. Where one nation has gotten
+    the start of another in trade, it is very difficult for the latter to regain the
+    ground it has lost; because of the superior industry and skill of the former,
+    and the greater stocks, of which its merchants are possessed, and which
+    enable them to trade on so much smaller profits. But these advantages are
+    compensated, in some measure, by the low price of labour in every nation
+    which has not an extensive commerce, and does not much abound in gold
+    and silver. Manufactures, therefore gradually shift their places, leaving
+    those countries and provinces which they have already enriched, and
+    flying to others, whither they are allured by the cheapness of provisions
+    and labour; till they have enriched these also, and are again banished by
+    the same causes.")
   }
 ])
