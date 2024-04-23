fbc08af7a9a89fa18e9415ac1cf23f5992b82512
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2024-04-23 08:18:20 +0200

    seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Article.create([
+  {
+    title: "Hossa na akcjach",
+    content: "There seems to be a happy concurrence of causes in human affairs, which
+    checks the growth of trade and riches, and hinders them from being
+    confined entirely to one people; as might naturally at first be dreaded from
+    the advantages of an established commerce. Where one nation has gotten
+    the start of another in trade, it is very difficult for the latter to regain the
+    ground it has lost; because of the superior industry and skill of the former,
+    and the greater stocks, of which its merchants are possessed, and which
+    enable them to trade on so much smaller profits. But these advantages are
+    compensated, in some measure, by the low price of labour in every nation
+    which has not an extensive commerce, and does not much abound in gold
+    and silver. Manufactures, therefore gradually shift their places, leaving
+    those countries and provinces which they have already enriched, and
+    flying to others, whither they are allured by the cheapness of provisions
+    and labour; till they have enriched these also, and are again banished by
+    the same causes."
+  },
+  {
+    title: "Bessa na akcjach",
+    content: "There seems to be a happy concurrence of causes in human affairs, which
+    checks the growth of trade and riches, and hinders them from being
+    confined entirely to one people; as might naturally at first be dreaded from
+    the advantages of an established commerce. Where one nation has gotten
+    the start of another in trade, it is very difficult for the latter to regain the
+    ground it has lost; because of the superior industry and skill of the former,
+    and the greater stocks, of which its merchants are possessed, and which
+    enable them to trade on so much smaller profits. But these advantages are
+    compensated, in some measure, by the low price of labour in every nation
+    which has not an extensive commerce, and does not much abound in gold
+    and silver. Manufactures, therefore gradually shift their places, leaving
+    those countries and provinces which they have already enriched, and
+    flying to others, whither they are allured by the cheapness of provisions
+    and labour; till they have enriched these also, and are again banished by
+    the same causes."
+  }
+])
