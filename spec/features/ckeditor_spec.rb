f2c737afa780285dd1596164698ea6b982c26aef
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2024-05-21 08:33:08 +0200

    views counter tests

--- spec/features/ckeditor_spec.rb
+++ spec/features/ckeditor_spec.rb
@@ -1,28 +1,26 @@
 # spec/features/ckeditor_spec.rb
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.feature "CKEditor", type: :feature, js: true do
-  let(:user) { create(:user, email: 'admin@example.com', password: 'password') }
+  let(:user) { create(:user, email: "admin@example.com", password: "password") }
 
   before do
     login_as(user, scope: :user)
   end
 
-  scenario 'User uses CKEditor in a form' do
+  scenario "User uses CKEditor in a form" do
     visit new_article_path
 
-    fill_in 'Title', with: 'Test Article'
+    fill_in "Title", with: "Test Article"
 
-    # Wait for CKEditor to initialize
-    expect(page).to have_css('.cke', visible: true)
+    expect(page).to have_css(".cke", visible: true)
 
-    # Set CKEditor content
     page.execute_script("CKEDITOR.instances['editor'].setData('<p>This is the content of the test article.</p>');")
 
-    click_button 'Create Article'
+    click_button "Create Article"
 
-    expect(page).to have_content('Article was successfully created.')
-    expect(page).to have_content('Test Article')
-    expect(page).to have_content('This is the content of the test article.')
+    expect(page).to have_content("Article was successfully created.")
+    expect(page).to have_content("Test Article")
+    expect(page).to have_content("This is the content of the test article.")
   end
 end
