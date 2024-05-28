077c0d6134f55d2377b23142a926a168fee62b8d
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2024-05-28 13:37:07 +0200

    image can be a content for validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,18 @@ class Comment < ApplicationRecord
   belongs_to :article
   belongs_to :user, optional: true
   has_rich_text :content
-  validates :content, presence: {message: "Treść komentarza nie może być pusta"}
+  validate :content_presence
   validates :username, presence: {message: "Nazwa użytkownika nie może być pusta"}, unless: -> { user.present? }
+
+  private
+
+  def content_presence
+    if content.blank? && !contains_image?
+      errors.add(:content, "Treść komentarza nie może być pusta")
+    end
+  end
+
+  def cointains_image?
+    content.to_plain_text.blank? && content.embeds.present?
+  end
 end
