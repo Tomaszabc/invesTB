2f17e6d87ee0f6732dc457ddd366f8988fc4a38a
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2024-06-04 19:11:01 +0200

    article thumbnail content strip_tags added

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,9 +1,11 @@
 module ArticlesHelper
+  include ActionView::Helpers::SanitizeHelper
+
   def render_article_content(article, length: 150)
     return "Brak treści" unless article&.content
 
-    content_text = article.content
-    truncated_content = truncate(content_text, length: length, escape: false)
-    raw(truncated_content)
+    content_text = strip_tags(article.content)
+    truncated_content = truncate(content_text, length: length)
+    truncated_content
   end
 end
