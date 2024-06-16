9293b856a1526fa2961e6e4baf65a1990635dbbc
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2024-06-16 09:33:34 +0200

    comments img captions removed, timestamp added

---
+++ app/javascript/trix/trix_custom_config.js
@@ -0,0 +1,8 @@
+document.addEventListener("trix-attachment-add", function(event) {
+  const attachment = event.attachment;
+  if (attachment.file) {
+    attachment.setAttributes({
+      caption: ""
+    });
+  }
+});
