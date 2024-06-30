d7a9aac77dec7178f13b5c08e3d71d9e987a5a09
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2024-06-30 21:19:53 +0200

    simpified application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,14 +8,10 @@ import "trix_custom_config";
 
 Rails.start();
 
-function initializeLightbox() {
-  console.log('Initializing Lightbox');
-
+document.addEventListener('turbo:load', function() {
   const images = document.querySelectorAll('.article-content img');
   images.forEach(image => {
     if (!image.parentNode.matches('a[data-lightbox]')) {
-      console.log('Wrapping image:', image);
-
       const anchor = document.createElement('a');
       anchor.href = image.src;
       anchor.setAttribute('data-lightbox', 'article-images');
@@ -24,53 +20,6 @@ function initializeLightbox() {
       }
       image.parentNode.insertBefore(anchor, image);
       anchor.appendChild(image);
-
-      console.log('Image wrapped:', image);
-    } else {
-      console.log('Image already wrapped:', image);
     }
   });
-
-  if (typeof lightbox !== 'undefined' && lightbox.init) {
-    console.log('Reinitializing Lightbox');
-    lightbox.init();
-  } else {
-    console.error('Lightbox is not defined or lightbox.init is not a function');
-  }
-
-  // Manually bind click events to ensure they trigger Lightbox
-  $('a[data-lightbox]').off('click').on('click', function(event) {
-    console.log('Lightbox link clicked:', this);
-    event.preventDefault(); // Prevent the default link behavior
-    lightbox.start($(this));  // Manually start Lightbox using jQuery
-  });
-}
-
-function setupLightboxObserver() {
-  const targetNode = document.querySelector('.article-content');
-  if (!targetNode) return;
-
-  const config = { childList: true, subtree: true };
-  const callback = function(mutationsList) {
-    for (const mutation of mutationsList) {
-      if (mutation.type === 'childList') {
-        initializeLightbox();
-      }
-    }
-  };
-
-  const observer = new MutationObserver(callback);
-  observer.observe(targetNode, config);
-}
-
-document.addEventListener('turbo:load', function() {
-  console.log('Turbo load event detected');
-  initializeLightbox();
-  setupLightboxObserver();
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('DOMContentLoaded event detected');
-  initializeLightbox();
-  setupLightboxObserver();
 });
