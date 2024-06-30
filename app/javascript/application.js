1a9ed43ba6ddc926217ee342b03c4e4337c025af
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2024-06-30 20:32:25 +0200

    removing of import * as lightbox from 'lightbox2' fix lightbox whole site scan but it breaks turbo redirection

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,7 +6,24 @@ import "@rails/actiontext";
 import "trix_custom_toolbar";
 import "trix_custom_config";
 
-import $ from "jquery";
-import * as lightbox from "lightbox2";  
+
+
+import * as lightbox from "lightbox2";
 
 Rails.start();
+
+document.addEventListener('turbo:load', function() {
+  const images = document.querySelectorAll('.article-content img');
+  images.forEach(image => {
+    if (!image.parentNode.matches('a[data-lightbox]')) {
+      const anchor = document.createElement('a');
+      anchor.href = image.src;
+      anchor.setAttribute('data-lightbox', 'article-images');
+      if (image.alt) {
+        anchor.setAttribute('data-title', image.alt);
+      }
+      image.parentNode.insertBefore(anchor, image);
+      anchor.appendChild(image);
+    }
+  });
+});
