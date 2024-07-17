3ca2bb1758e3bd8aaf331a94b41d020e4916bb01
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2024-07-17 11:08:49 +0200

    styling for load more button

--- app/javascript/controllers/articles_controller.js
+++ app/javascript/controllers/articles_controller.js
@@ -1,30 +1,48 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["articles", "loadMoreButton"]
+  static targets = ["articles", "loadMoreButton", "loader"]
 
   connect() {
     this.offset = 6;
   }
 
   loadMoreArticles() {
-    fetch(`/articles/load_more?offset=${this.offset}`)
-      .then(response => response.text())
-      .then(html => {
+    this.showLoader();
+    const fetchArticles = fetch(`/articles/load_more?offset=${this.offset}`)
+      .then(response => response.text());
+
+    const delay = new Promise(resolve => setTimeout(resolve, 500)); // 500ms delay
+
+    Promise.all([fetchArticles, delay])
+      .then(([html]) => {
         this.articlesTarget.insertAdjacentHTML('beforeend', html);
         this.offset += 6;
 
         const moreArticlesFlag = document.querySelector('#more-articles-flag');
-        const moreArticles = moreArticlesFlag.dataset.moreArticles === 'true';
-
-        if (!moreArticles) {
-          this.loadMoreButtonTarget.textContent = 'To wszystkie artykuły';
-          this.loadMoreButtonTarget.disabled = true;
+        if (moreArticlesFlag) {
+          const moreArticles = moreArticlesFlag.dataset.moreArticles === 'true';
+          if (!moreArticles) {
+            this.loadMoreButtonTarget.textContent = 'To wszystkie artykuły';
+            this.loadMoreButtonTarget.disabled = true;
+          }
+          moreArticlesFlag.remove(); // Remove the flag element after checking
         }
-
-        // Remove the flag element after checking
-        moreArticlesFlag.remove();
+        this.hideLoader();
       })
-      .catch(error => console.error('Error loading more articles:', error));
+      .catch(error => {
+        console.error('Error loading more articles:', error);
+        this.hideLoader();
+      });
+  }
+
+  showLoader() {
+    this.loadMoreButtonTarget.style.display = 'none';
+    this.loaderTarget.style.display = 'block';
+  }
+
+  hideLoader() {
+    this.loaderTarget.style.display = 'none';
+    this.loadMoreButtonTarget.style.display = 'block';
   }
 }
