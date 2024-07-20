d255260325e9eb3b8d43aa033963ff92a98d5d53
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2024-07-20 19:17:26 +0200

    Working commit before Editor JS

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,10 +1,11 @@
 module ArticlesHelper
-  include ActionView::Helpers::SanitizeHelper
-
-  def render_article_content(article, length: 150)
+  def render_article_content(article, length: 100)
     return "Brak treści" unless article&.content
 
     content_text = strip_tags(article.content)
-    truncate(content_text, length: length)
+    sanitized_content = self.class.full_sanitizer.sanitize(content_text)
+    truncated_content = truncate(sanitized_content, length: length)
+
+    truncated_content.html_safe
   end
 end
