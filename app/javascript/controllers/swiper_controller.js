106681e06096f94b27445eb10a4b130af310d479
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2024-08-19 21:02:58 +0200

    javascript fix

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -1,26 +1,30 @@
-// app/javascript/controllers/swiper_controller.js
 import { Controller } from "@hotwired/stimulus";
+import Swiper from "swiper/bundle";
 
 export default class extends Controller {
   connect() {
-    this.swiper = new Swiper('.swiper', {
-      // Optional parameters
-      direction: 'horizontal',
-      loop: true,
-      spaceBetween: 120,
-      // If we need pagination
-      pagination: {
-        el: '.swiper-pagination',
-      },
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      // And if we need scrollbar
-      scrollbar: {
-        el: '.swiper-scrollbar',
-      },
+    document.addEventListener("turbo:load", () => {
+      this.initializeSwiper();
     });
+  }
+
+  initializeSwiper() {
+    if (!this.swiper) {
+      this.swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+        spaceBetween: 120,
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
+    }
   }
 }
