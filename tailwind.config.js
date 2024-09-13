7bbbaacf9ea479ccb27c41852c3a0e74c650809f
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2024-09-13 16:36:31 +0200

    steps 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
+    './app/assets/stylesheets/components/**/*.css',
     './app/javascript/**/*.js'
   ]
 }
