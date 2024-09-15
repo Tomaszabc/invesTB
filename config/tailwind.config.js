dc7cf80af3bd56009a672824c3c5ea205e42c36b
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2024-09-15 10:16:27 +0200

    styling for ckeditor

---
+++ config/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: false,
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/assets/stylesheets/components/**/*.css',
+    './app/javascript/**/*.js'
+  ]
+}
